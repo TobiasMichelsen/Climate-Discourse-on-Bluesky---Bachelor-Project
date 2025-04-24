4c187ce20709c30746141a8df4522af756f13d51
abel <abelszemler.off@gmail.com>
2025-04-24 20:17:49 +0200

    abel annot

--- hpc_scripts/LLM/Minstral/HPC_LLM_test.py
+++ hpc_scripts/LLM/Minstral/HPC_LLM_test.py
@@ -3,11 +3,24 @@ import os
 import pandas as pd
 import pickle
 import datetime
+from huggingface_hub import login
 
 input_path = "../..//data/climate_classified"
 keep_running = True
 counter = 0
 concat_df = pd.DataFrame()
+
+#Initialize mdoel
+HUGGINGFACE_TOKEN = "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"
+print(f"Initializing model..", flush=True)
+login(token=HUGGINGFACE_TOKEN)
+model_id = "mistralai/Mistral-7B-Instruct-v0.1"  # Or LLaMA, Zephyr, etc.
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
+
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+
 while keep_running:
     for filename in os.listdir(input_path):
         start_time = datetime.datetime.now()
@@ -19,17 +32,9 @@ while keep_running:
         df = pd.read_json(f"{input_path}/{filename}")    
         df = df[df["label"] == "yes"]
         df = df[df["score"] >= .99]
-
-        for elem in df:
-            text = elem["text"]
-
-            model_id = "mistralai/Mistral-7B-Instruct-v0.1"  # Or LLaMA, Zephyr, etc.
-            tokenizer = AutoTokenizer.from_pretrained(model_id)
-            model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
-
-            pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-
-            prompt = """
+        df_text = df["text"]
+        for text in df_text:
+            prompt = f"""
             You are a helpful assistant that classifies text into climate-related categories.
             Categories:
             1. Renewable Energy - e.g., "Solar panels are being installed across rooftops"
@@ -37,7 +42,7 @@ while keep_running:
             3. Agriculture - e.g., "Crops are failing due to drought"
 
             Classify the following:
-            f"{text}"
+            {text}
 
             Answer:
             """
@@ -45,8 +50,8 @@ while keep_running:
             response = pipe(prompt, max_new_tokens=100)[0]["generated_text"]
             print(f"input:{text}\noutput:{response}\n", flush = True)
             out_df = pd.DataFrame({"text": [text], "response": [response]})
-            concat_df = pd.concat(concat_df, out_df)
-        elapsed_time = datetime-datetime.now() - start_time
+            concat_df = pd.concat([concat_df, out_df])
+        elapsed_time = datetime.datetime.now() - start_time
         print(f"finished processing {filename}, processed {len(df)} rows\nit took: {elapsed_time}", flush=True)
 
 print(f"processed {counter} files", flush=True) 
