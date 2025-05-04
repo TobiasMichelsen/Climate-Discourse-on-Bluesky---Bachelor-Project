cd28bd54f2a149ae1fb90d55606aa968788cac49
TobiasMichelsen <tobias.h.z.michelsen@gmail.com>
2025-05-04 16:45:53 +0200

    hpc files, grids for run2,Classification struct

---
+++ hpc_scripts/Classification/BERTopic/Tobias_TEST/BERTopic_Hyperparameters/Run_2/Run_2_config_2_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=Run2_2            # Job name
+#SBATCH --output=Run2_2.%j.out       # Output file (%j = job ID)
+#SBATCH --error=Run2_2.%j.err        # Error file
+#SBATCH --cpus-per-task=4                    # Number of CPU cores
+#SBATCH --mem=32G                            # Memory
+#SBATCH --gres=gpu:v100:1                    # GPU request
+#SBATCH --time=36:00:00                      # Max run time
+#SBATCH --partition=acltr                    # Partition
+#SBATCH --mail-type=FAIL                     # Email on failure
+
+# Load CUDA
+module load CUDA/12.1.1
+
+# Load Python
+module load Python/3.12.3-GCCcore-13.3.0
+python --version
+hostname
+
+# Activate your environment
+source ../../../envs/BERTopic_env/bin/activate
+
+# Run the updated script
+python Run_2_config_2.py
---
+++ hpc_scripts/Classification/BERTopic/Tobias_TEST/BERTopic_Hyperparameters/Run_2/Run_2_fast_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=Run2_BERT_fast            # Job name
+#SBATCH --output=Run2_BERT_fast.%j.out       # Output file (%j = job ID)
+#SBATCH --error=Run2_BERT_fast.%j.err        # Error file
+#SBATCH --cpus-per-task=4                    # Number of CPU cores
+#SBATCH --mem=32G                            # Memory
+#SBATCH --gres=gpu:v100:1                    # GPU request
+#SBATCH --time=12:00:00                      # Max run time
+#SBATCH --partition=acltr                    # Partition
+#SBATCH --mail-type=FAIL                     # Email on failure
+
+# Load CUDA
+module load CUDA/12.1.1
+
+# Load Python
+module load Python/3.12.3-GCCcore-13.3.0
+python --version
+hostname
+
+# Activate your environment
+source ../../../envs/BERTopic_env/bin/activate
+
+# Run the updated script
+python Run_2_fast.py
